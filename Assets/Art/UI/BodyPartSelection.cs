46fce5ce39ff9d69560e5be83608b44b0f8e2b83
NicolasTheHuman <56463262+NicolasTheHuman@users.noreply.github.com>
2021-04-18 23:32:56 -0300

    UI
    Hice algunos menus de UI para ir probando.

---
+++ Assets/Art/UI/BodyPartSelection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BodyPartSelection : MonoBehaviour
+{
+	public Image squareBracket;
+
+	public void GenerateBracket()
+	{
+		var myButton = EventSystem.current.currentSelectedGameObject.transform;
+		var tempSquareBracket = Instantiate(squareBracket);
+		tempSquareBracket.transform.SetParent(myButton, false);
+		tempSquareBracket.rectTransform.anchorMin = new Vector2(0, 0);
+		tempSquareBracket.rectTransform.anchorMax = new Vector2(1, 1);
+	}
+}
