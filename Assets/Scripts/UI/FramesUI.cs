e0e2a8400bc66908752113c5dc52e8f15b2963f7
NicolasTheHuman <56463262+NicolasTheHuman@users.noreply.github.com>
2021-06-18 04:20:38 -0300

    Prototipo interfaz de orden de los turnos

---
+++ Assets/Scripts/UI/FramesUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FramesUI : MonoBehaviour
+{
+	public Image mechaImage;
+	public Image leftGunIcon;
+	public Image rightGunIcon;
+	public TextMeshProUGUI mechaName;
+	
+	FramesUI(Image mecha, Image leftGun, Image rightGun, TextMeshProUGUI myName)
+	{
+		mechaImage = mecha;
+		leftGunIcon = leftGun;
+		rightGunIcon = rightGun;
+		mechaName = myName;
+	}
+
+	public void ChangeData(Image newMechaImage, Image newMechaLeftIcon, Image newMechaRightIcon, TextMeshProUGUI newMechaName)
+	{
+		mechaImage = newMechaImage;
+		leftGunIcon = newMechaLeftIcon;
+		rightGunIcon = newMechaRightIcon;
+		mechaName.text = newMechaName.text;
+	}
+
+	public void ChangeName(TextMeshProUGUI newName)
+	{
+		mechaName.text = newName.text;
+	}
+	/*public FramesUI ChangeData(FramesUI newFrame)
+	{
+		return new FramesUI(newFrame.mechaImage, newFrame.leftGunIcon, newFrame.rightGunIcon, newFrame.mechaName);
+	}*/
+}
