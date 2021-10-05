c4b233b90efd35e0cfc06c7306c3533f71dec8ca
NicolasTheHuman <56463262+NicolasTheHuman@users.noreply.github.com>
2021-10-05 19:49:33 -0300

    Level 2 y 3

--- Assets/Scripts/Utilities/ChangeScene.cs
+++ Assets/Scripts/Utilities/ChangeScene.cs
@@ -25,9 +25,18 @@ public class ChangeScene : MonoBehaviour
 
     public void LoadGame()
     {
-        SceneManager.LoadScene("Level");
+        SceneManager.LoadScene("TEST 1");
     }
 
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
     public void LoadWorkShop()
     {
         SceneManager.LoadScene("TallerScene");
