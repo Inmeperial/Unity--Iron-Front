7d1ff9020db993cd453060f705887cfdfbcfb4d0
NicolasTheHuman <56463262+NicolasTheHuman@users.noreply.github.com>
2021-11-04 20:26:06 -0300

    Cambios canvas

---
+++ Assets/CanvasWeight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasWeight : MonoBehaviour
+{
+    public int currentWeight, maxWeight;
+    public int minZRot, maxZRot;
+    int currentRotation;
+    RectTransform myRect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRect = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateRotation()
+	{
+        currentRotation = currentWeight / maxWeight;
+        Debug.Log("Current Rot = " + currentRotation);
+	}
+}
