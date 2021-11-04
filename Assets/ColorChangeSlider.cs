533985b377321d725ebf3e7dde485a964bb0758b
NicolasTheHuman <56463262+NicolasTheHuman@users.noreply.github.com>
2021-11-03 23:26:04 -0300

    Customize tab y sliders de colores

---
+++ Assets/ColorChangeSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorChangeSlider : MonoBehaviour
+{
+
+    public Slider red, green, blue;
+    public Image image;
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.color = new Color(red.value, green.value, blue.value);
+    }
+}
