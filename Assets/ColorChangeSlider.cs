fc15f6ac9ffa4d1aff52f3f62689cabc7bea31fa
NicolasTheHuman <56463262+NicolasTheHuman@users.noreply.github.com>
2021-11-13 19:26:38 -0300

    Agregue los botones para copiar los valores de un slider a otro

--- Assets/ColorChangeSlider.cs
+++ Assets/ColorChangeSlider.cs
@@ -8,10 +8,18 @@ public class ColorChangeSlider : MonoBehaviour
 
     public Slider red, green, blue;
     public Image image;
+    public ColorChangeSlider otherSlider;
 
     // Update is called once per frame
     void Update()
     {
         image.color = new Color(red.value, green.value, blue.value);
     }
+
+    public void ChangeOtherSlider()
+	{
+        otherSlider.red.value = red.value;
+        otherSlider.green.value = green.value;
+        otherSlider.blue.value = blue.value;
+	}
 }
