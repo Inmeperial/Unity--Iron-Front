e33a51e9db74e3463983d11327f57c732166a822
Inmeperial <quique_12_04@hotmail.com>
2021-11-26 16:50:40 -0300

    Sounds update, new Shader and new Scene "CityScene"
    Sounds added in Level2
    Sound WaterFlowFx edited for normalized and added to unity.
    New shader for OutLine
    New Scene CityScene

---
+++ Assets/Scripts/Managers/MouseActionInLevelTransition.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MouseActionInLevelTransition : MonoBehaviour
+{
+    public bool allReadyPlayed = false; //so we dont re play the same level, need to make the texture in gray.
+    [SerializeField] private LevelEnumList levelToLoad;
+    private Material _mat = default;
+
+    void Start()
+    {
+        _mat = this.GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == this.transform)
+                {
+                    if (levelToLoad == LevelEnumList.LevelWS || !allReadyPlayed)
+                    {
+                        LoadScene(levelToLoad);
+                    }
+                }
+            }
+        }
+    }
+
+    private void LoadScene(LevelEnumList levelEnum)
+    {
+        switch (levelEnum)
+        {
+            case LevelEnumList.Level1:
+                SceneManager.LoadScene("Level 1");
+                break;
+            case LevelEnumList.Level2:
+                SceneManager.LoadScene("Level 2");
+                break;
+            case LevelEnumList.Level3:
+                SceneManager.LoadScene("Level 3");
+                break;
+            case LevelEnumList.LevelWS:
+                SceneManager.LoadScene("TallerScene");
+                break;
+            default:
+                SceneManager.LoadScene("Level 1");
+                break;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (!allReadyPlayed || levelToLoad == LevelEnumList.LevelWS)
+        {
+            SetOutLine(true);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (!allReadyPlayed || levelToLoad == LevelEnumList.LevelWS)
+        {
+            SetOutLine(false);
+        }
+    }
+
+    private void SetOutLine(bool key)
+    {
+        _mat.SetInt("_isOutLineOn", key ? 1 : 0);
+    }
+
+    private enum LevelEnumList
+    {
+        Level1,
+        Level2,
+        Level3,
+        LevelWS,
+    }
+
+}
---
+++ Assets/Scripts/UI/MenuOptionsInGame.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MenuOptionsInGame : MonoBehaviour
+{
+    public Text textVolume = default;
+
+    [SerializeField] private GameObject _menuInGameObj = default;
+    [SerializeField] private GameObject _optionsObj = default;
+    [SerializeField] private GameObject _devObj = default;
+    [SerializeField] private AudioMixer _audioMixer = default;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_menuInGameObj.activeSelf)
+            {
+                CloseAllMenu();
+            }
+            else
+            {
+                OpenAllMenu();
+            }
+        }
+    }
+
+    public void CloseAllMenu()
+    {
+        _audioMixer.SetFloat("pitch", 1);
+        _menuInGameObj.SetActive(false);
+        CloseOptionsMenu();
+        CloseDevMenu();
+    }
+
+    private void OpenAllMenu()
+    {
+        _audioMixer.SetFloat("pitch", 0.3f);
+        _menuInGameObj.SetActive(true);
+    }
+
+    public void SetVolume(float vol) //Used on Inspector
+    {
+        float numPercentage = vol * 100;
+        float volumeToSet = ((((numPercentage * 80) / 100) * -1) + 80) * -1;
+        textVolume.text = Mathf.RoundToInt(numPercentage) + "%";
+        _audioMixer.SetFloat("volume", volumeToSet);
+    }
+
+    public void CloseOptionsMenu()
+    {
+        if (_optionsObj)
+            _optionsObj.SetActive(false);
+    }
+
+    public void CloseDevMenu()
+    {
+        if (_devObj)
+            _devObj.SetActive(false);
+    }
+}
