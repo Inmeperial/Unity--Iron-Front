0015522f6846fc64ae843b229976ad63ca6fdee1
Leandro Biloni <leandrobiloni@hotmail.com>
2022-12-12 14:47:38 -0300

    Fix tooltip mostraba data cuando no habia nada

--- Assets/Project/Scripts/UI/PlayerHUD/EquipablesHUD.cs
+++ Assets/Project/Scripts/UI/PlayerHUD/EquipablesHUD.cs
@@ -143,6 +143,8 @@ public class EquipablesHUD : Initializable
         button.SetButtonIcon(_noneIcon);
         button.HideAbilityCooldownText();
         button.HideItemUsesText();
+
+        button.GetComponent<OverviewData>().SetTooltipData();
     }
 
     private void DeactivateButtons()
