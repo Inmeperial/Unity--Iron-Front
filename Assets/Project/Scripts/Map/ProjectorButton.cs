f0c7b1344b2b518aa69515f844b96ae3c6d5e5bf
Leandro Biloni <leandrobiloni@hotmail.com>
2022-12-23 18:06:16 -0300

    Botones al mapa

--- Assets/Project/Scripts/Map/ProjectorButton.cs
+++ Assets/Project/Scripts/Map/ProjectorButton.cs
@@ -4,21 +4,35 @@ using UnityEngine.EventSystems;
 public class ProjectorButton : CustomButton
 {
     [SerializeField] private MissionDataSO _missionData;
+    [SerializeField] private float _hoverScaleMultiplier = 1.5f;
 
     private MissionProjector _projector;
+
+    private Vector3 _originalScale;
+
     protected override void Awake()
     {
+        _originalScale = targetGraphic.transform.localScale;
         _projector = FindObjectOfType<MissionProjector>();
     }
 
     public override void OnPointerEnter(PointerEventData eventData)
     {
         base.OnPointerEnter(eventData);
+
+        targetGraphic.color = colors.highlightedColor;
+
+        targetGraphic.transform.localScale *= _hoverScaleMultiplier;
+
         _projector.ChangeMissionImage(_missionData);
     }
 
     public override void OnPointerExit(PointerEventData eventData)
     {
         base.OnPointerExit(eventData);
+
+        targetGraphic.transform.localScale = _originalScale;
+
+        targetGraphic.color = colors.normalColor;
     }
 }
